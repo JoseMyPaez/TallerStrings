0a4b38f9de998c60dc953fab6a15f22823fbf5b7
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-10-14 19:35:36 +0000

    sdasd

--- firstExercice.cpp
+++ firstExercice.cpp
@@ -2,14 +2,6 @@
 
 using namespace std;
 
-void forWithString(){
-    string example = "Hello World";
-    for (int i = 0; i > example.size(); i++)
-    {
-        char textChar = example[i];
-        cout<<"position : "<< i << " - "<<textChar<<endl;
-    }  
-}
 
 void forWithStringVocals(){
     string example = "Hello World";
@@ -23,49 +15,6 @@ void forWithStringVocals(){
         }  
     }   
     cout<<"hay "<<vocaksCount<<" vocales"<<endl;
-}
-
-void forWhitStringALetter(){
-    string example = "otorrinolaringologo";
-    int vocaksCount = 0;
-    for (int i = 0; i < example.size(); i++)
-    {
-        char textChar = example[i];
-        if (textChar == 'o')
-        {
-            vocaksCount++;
-        }  
-    }   
-    cout<<"hay "<<vocaksCount<<" letras o "<<endl;
-}
-
-void replaceSpaces(){
-    string example = "Hello World";
-    string news = "";
-    for (int i = 0; i < example.size(); i++)
-    {
-        char textChar = example[i];
-        if (textChar != ' ')
-        {
-            news += textChar;
-        } else {
-            news += '-';
-        }
-    }   
-    cout<<example<<endl;
-}  
-
-void duplicateStringLetters(){
-    string example = "Hello World";
-    string news = "";
-    for (int i = 0; i < example.size(); i++)
-    {
-        char textChar = example[i];
-        news += textChar;
-        news += textChar;
-        
-    }   
-    cout<<news<<endl;
 }
 
 void removeVowels(){
@@ -82,30 +31,14 @@ void removeVowels(){
         }
     }
     cout<<news<<endl;
-}
 
-void invertString(){
-    string example = "Hello World";
-    string news = "";
-    for (int i = example.size() - 1; i >= 0; i--)
-    {
-        char textChar = example[i];
-        news += textChar;
-    }
-    cout<<news<<endl;
-}
-    
+
 
 
 
 int main (){
-    //forWithString();
-    //forWithStringVocals();
-    //forWhitStringALetter();
-    //replaceSpaces();
-    //duplicateStringLetters();
+
     removeVowels();
-    invertString();
 
 
     return 0;
